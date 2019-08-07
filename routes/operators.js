ceacb0332b208e276ba59b05bcf9fcfb14946ad1
Guozhi Tang <tangguozhi53@gmail.com>
2019-08-07 11:08:38 -0400

    Update some of login system, not completed

--- routes/operators.js
+++ routes/operators.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 // Bring in our models
 const Operator = require('../models/operator');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 // To get all Operators
 router.get('/getoperators', (req, res, next) => {
@@ -54,5 +56,55 @@ router.post('/searchbyname', (req, res, next) => {
     }
   });
 });
+
+// To authenticate that whether the login operator has same information with exusted one.
+router.post('/authenticate', (req, res, next) => {
+  // res.send('AUTHENTICATE');
+  const name = req.body.name;
+  
+  Operator.getByName(name, (err, operator) => {
+    // console.log(operator[0].name);
+    if (err) {
+      res.json({success: false, msg:'Error exists for authentication!'});
+    } else if (!operator) {
+      res.json({success: false, msg:'Operator not found!'});
+    } else if (operator[0].name != name) {
+      res.json({success: false, msg:'Name not exists in database!'});
+    } else if (operator[0].name == name) {
+      const token = jwt.sign({data: operator[0]}, config.secret, {
+        expiresIn: 604800 // One week
+      });
+
+      res.json({
+        success: true,
+        token: 'JWT ' + token,
+        operator: {
+          id: operator._id,
+          name: operator.name
+        },
+        msg:'Login Successfully!'
+      });
+    }
+  });
+});
+
+// Profile - /users/profile
+// one of the routes we will protect with our aithenticate with our token
+// router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+//   // res.send('PROFILE');
+//   // res.json({msg: "success profile"});
+//   res.json({user: req.user});
+// });
+
+// router.get('/nulluser', (req, res, next) => {
+//   const name = undefined;
+//   const department = undefined;
+//   const user = {
+//     name: name,
+//     department: department
+//   }
+
+//   res.json({user: user});
+// });
 
 module.exports = router;
