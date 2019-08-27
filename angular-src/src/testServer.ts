544650025bdfe4fe5aef3d4c9888e1bb57826f7e
Guozhi Tang <tangguozhi53@gmail.com>
2019-08-27 11:01:34 -0400

    New version completed

--- angular-src/src/testServer.ts
+++ angular-src/src/testServer.ts
@@ -1,3 +1,5 @@
 // When in development mode, the address for Node Server could be defined exactly 
-export const testServer = 'http://localhost:3000/';
-// export const testServer = '';
+// export const testServer = 'http://localhost:3000/';
+
+// For Production mode
+export const testServer = '';
